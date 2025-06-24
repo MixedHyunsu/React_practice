f6e112b913370522072b8a8a461cf1557a96eb5b
MixedHyunsu <hyunsu@Margherita.local>
2025-06-24 12:18:09 +1000

    Practice using const, function componant

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+	  <div className="container test">
+		<Container count2={count} setCount={setCount} />
+	  </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
@@ -32,4 +35,31 @@ function App() {
   )
 }
 
+const Container = () => {
+	const [count, setCount] = useState(0)
+	return (
+		<div>
+			<Button count={count} setCount={setCount} />
+			<Counter count={count} />
+		</div>
+	)
+}
+
+function Button({ count, setCount }) {
+	return (
+		<div>
+			<button id='button' onClick={() => setCount(count + 1)}>
+				Click me
+			</button>
+		</div>
+	)
+}
+
+function Counter({count}) {
+	return (
+		<em>
+			<p id='counter'>You clicked {count} times</p> 
+		</em>
+	)
+}
 export default App
